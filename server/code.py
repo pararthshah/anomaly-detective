c6ede062ead1bb754d708aee6805607ebe13c1c6
Vikesh Khanna <khanna.vikesh@gmail.com>
2014-05-01 03:47:53 -0700

    Dummy ajax call to webpy server to test end-end pipeline and chart updation

--- server/code.py
+++ server/code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import web
+import json
 
 urls = (
     '/', 'Index',
@@ -21,8 +22,12 @@ class Metrics:
 
 class Data:
     def GET(self):
-        params = web.input()
-        return "Data!"
+				params = web.input()
+				web.header('Content-Type', 'application/json')
+        # TODO: This is needed for CORS (AJAX)- Currently, our HTTP and
+				# API servers are on different domains. Consider removing/refactoring.
+				web.header('Access-Control-Allow-Origin', '*')
+				return json.dumps([[5, 2], [6, 3], [8, 2], [10,5], [16,8],[29,10]]);
 
 class Anomalies:
     def GET(self):
