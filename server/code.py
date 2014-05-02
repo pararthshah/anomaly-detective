60ab461e1e72771c76b6071a145714d3b8a19bfa
Pararth Shah <pararthshah717@gmail.com>
2014-05-01 20:49:25 -0700

    many upates

--- server/code.py
+++ server/code.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
+import config
+import sys
+
+sys.path.insert(0,"../scripts")
+
 import web
 import os, sys, time
 import json
-from scripts import read_timeseries
+import read_timeseries
 
 rel_datapath= "../../cs341/data/week1"
 
@@ -19,7 +24,7 @@ render = web.template.render('templates')
 
 class Index:
     def GET(self):
-        return render.test('Bob')
+        return render.index('Bob')
 
 class Metrics:
     def GET(self):
@@ -28,16 +33,16 @@ class Metrics:
 
 class Data:
     def GET(self):
-	params = web.input()
-	web.header('Content-Type', 'application/json')
-	# TODO: This is needed for CORS (AJAX)- Currently, our HTTP and
-	# API servers are on different domains. Consider removing/refactoring.
-	web.header('Access-Control-Allow-Origin', '*')
-	filename= params.machine + "." + params.metric
-	path= os.path.join(os.getcwd(), rel_datapath, "json", filename)
-	with open(path) as str_file:
-		json_str= str_file.read()
-	return json_str
+        params = web.input()
+        web.header('Content-Type', 'application/json')
+        # TODO: This is needed for CORS (AJAX)- Currently, our HTTP and
+        # API servers are on different domains. Consider removing/refactoring.
+        #web.header('Access-Control-Allow-Origin', '*')
+        filename= params.machine + "." + params.metric
+        path= os.path.join(os.getcwd(), rel_datapath, "json", filename)
+        with open(path) as str_file:
+            json_str= str_file.read()
+            return json_str
 
 
 class Anomalies:
@@ -48,12 +53,11 @@ class Anomalies:
 class Annotations:
     def POST(self):
         json_data = web.data()
-	# get 'name' from json_data
-	name= json.loads(json_data)['name']
-	path= os.path.join(rel_datapath, "annotaitions", name + time.time())
-	with open(path, 'w') as annotation_file:
-		annotation_file.write(json_data)
-
+        # get 'name' from json_data
+        name= json.loads(json_data)['name']
+        path= os.path.join(rel_datapath, "annotations", name + time.time())
+        with open(path, 'w') as annotation_file:
+            annotation_file.write(json_data)
         return "Annotations!"
 
 if __name__ == "__main__": 
---
+++ server/config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import json
+
+SERVER_DIR = os.path.abspath(os.path.dirname(__file__))
+
+BASE_DIR = os.path.abspath(os.path.join(SERVER_DIR, os.pardir))
+
+SCRIPTS_DIR = os.path.join(BASE_DIR, "scripts")
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+TS_DIR = os.path.join(DATA_DIR, "timeseries")
+
+ANOMALIES_DIR = os.path.join(DATA_DIR, "anomalies")
+
+ANNOTATIONS_DIR = os.path.join(DATA_DIR, "annotations")
+
+f = open(os.path.join(DATA_DIR, "metrics.json"))
+
+METRICS_LIST = json.load(f)
+
+f.close()
+
+del os
